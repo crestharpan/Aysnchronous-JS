05c8f773ba7477c5aca682775b34abd79895c512
crestharpan <arpanshrestha225@gmail.com>
2024-08-05 13:00:35 +0545

    API CALL WITH XML

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+//xml http request(old school way of doing AJAX)
+const request = new XMLHttpRequest();
